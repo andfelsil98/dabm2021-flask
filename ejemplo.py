ce7e7942dab2d3a3e3b878a54710c38e11dc6dbe
andfelsil98 <and_fel_sil1998@hotmail.com>
2021-10-29 16:57:56 -0500

    version 1.0

---
+++ ejemplo.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,request
+
+
+app = Flask(__name__)
+
+@app.route('/')  ##le estoy diciendo a flask cuando el usuario entre a esta ruta ejecuta esta funcion
+def inicio():
+    return render_template('login.html')
+
+@app.route('/datos')
+def datos():
+    user = {'nombre' : 'david'}
+    return render_template('datos.html',title='Titulo personalizado', user = user)
+
+@app.route('/validar', methods = ['POST'])   ###metodo get en la etiqueta form de html me sirve para capturar los datos y poder visualizarlos en el url, con el metodo post puedo guardar esos datos pero no visualizarlos en el url para mayor seguridad
+def validar(): ##MENSAJE QUE SE ENVIA al servidor para verificar informacion que captura se llama request
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        password = request.form['password']
+        resultado = verificar(usuario,password)
+        return resultado
+
+def verificar(usuario,password):
+    file = open('C:/Users/Administrador/Desktop/DABM PYTHONv2/flask/bd/users.csv','r')
+    datos = file.readlines()
+    file.close()
+    for dato in datos:
+        dato = dato.replace('\n','')
+        us,contra = dato.split(';')
+        
+        if us == usuario and contra == password:
+            return 'Usuario autenticado exitosamente'
+        else:
+            print(usuario,password)
+            return 'Usuario no registrado'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # validar()
